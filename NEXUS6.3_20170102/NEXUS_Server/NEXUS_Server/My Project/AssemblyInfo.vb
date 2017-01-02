999ed8eebbeae90167e6d73e05ea5da1ed71fdc4
AesirTitan <ayan.bifrost@gmail.com>
2017-01-02 22:28:21 +0800

    20170102

---
+++ NEXUS6.3_20170102/NEXUS_Server/NEXUS_Server/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' 有关程序集的常规信息通过下列特性集
+' 控制。更改这些特性值可修改
+' 与程序集关联的信息。
+
+' 查看程序集特性的值
+
+<Assembly: AssemblyTitle("NEXUS_Server")> 
+<Assembly: AssemblyDescription("NEXUS_Server")>
+<Assembly: AssemblyCompany("Titan Studio™")>
+<Assembly: AssemblyProduct("NEXUS（魔枢）")>
+<Assembly: AssemblyCopyright("Copyright © Titan Studio 2017")>
+<Assembly: AssemblyTrademark("Titan Studio™")>
+
+<Assembly: ComVisible(True)> 
+
+'如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+<Assembly: Guid("a9363905-9bbb-4ee2-bf80-53f655aac207")>
+
+' 程序集的版本信息由下面四个值组成: 
+'
+'      主版本
+'      次版本
+'      生成号
+'      修订号
+'
+' 可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
+' 方法是按如下所示使用“*”: 
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("6.3.0.5")>
+<Assembly: AssemblyFileVersion("6.3.0.5")>
---
+++ NEXUS6.3_20170102/NEXUS_Tips/Tips_Error/Tips_Error.xaml.vb
@@ -0,0 +1,18 @@
+﻿Class MainWindow
+    Dim WshShell = CreateObject("WScript.Shell")
+   
+    Private Sub MainWindow_Initialized(sender As Object, e As EventArgs) Handles Me.Initialized
+       
+    End Sub
+
+ 
+    Private Sub ButtonOK_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles ButtonOK.MouseDown
+
+        WshShell.run("https://TitanStudio.github.io/NEXUS")
+        End
+    End Sub
+
+    Private Sub ButtonCancel_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles ButtonCancel.MouseDown
+        End
+    End Sub
+End Class
