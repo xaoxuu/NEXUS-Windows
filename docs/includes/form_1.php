5bc5ca6e6280aa844920faf645d559656279b5b0
AesirTitan <ayan.bifrost@gmail.com>
2017-02-01 20:49:51 +0800

    20170201

--- docs/includes/form_1.php
+++ docs/includes/form_1.php
@@ -6,14 +6,14 @@
 	
 	$email = $_POST['email'];
 	
-	$to = 'ayan.x@outlook.com'; // Email submissions are sent to this email
+	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from nexus.";
 	$email_body = "You have received a new message. \n\n".
 				  "Email: $email \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
-	$headers .= "From: contact@xaoxuu.com\n";
+	$headers .= "From: contact@yoursite.com\n";
 	$headers .= "Reply-To: $email";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
